63cacdf012e52b4c90084c329b8e7e0f1c768ac9
Cachoulet Clement <clementcachoulet@orange.fr>
2020-09-28 22:50:27 +0200

    add content on show page

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -11,12 +12,22 @@ class AdController extends AbstractController
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo)
     {
-        $repo = $this->getDoctrine()->getRepository(Ad::class);
         $ads = $repo->findAll();
         return $this->render('ad/index.html.twig', [
             'ads' => $ads
+        ]);
+    }
+
+    /**
+     * @Route("/ads/{slug}", name="ads_show")
+     */
+    public function show($slug, AdRepository $repo) {
+        // Je récupère l'annonce qui correspond au slug 
+        $ad = $repo->findOneBySlug($slug);
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
         ]);
     }
 }
