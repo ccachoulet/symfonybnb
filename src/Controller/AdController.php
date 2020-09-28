c20faf909fd059d6ac2a87cfefb7cefc1de750b2
Cachoulet Clement <clementcachoulet@orange.fr>
2020-09-28 21:02:37 +0200

    create ads view

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,18 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Ad;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdController extends AbstractController
 {
     /**
-     * @Route("/ad", name="ad")
+     * @Route("/ads", name="ads_index")
      */
     public function index()
     {
+        $repo = $this->getDoctrine()->getRepository(Ad::class);
+        $ads = $repo->findAll();
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+            'ads' => $ads
         ]);
     }
 }
--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Cocur\Slugify\Slugify;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-// use App\Repository\AdRepository;
+use App\Repository\AdRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
