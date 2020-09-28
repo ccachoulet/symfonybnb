9626b86aac01a50981f9be2ae30fbd0405b29320
Cachoulet Clement <clementcachoulet@orange.fr>
2020-09-28 19:14:15 +0200

    create AdController

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Faker\Factory;
@@ -30,6 +31,16 @@ class AppFixtures extends Fixture
             ->setContent($content) 
             ->setPrice(mt_rand(40, 200))
             ->setRooms(mt_rand(1, 5));         
+
+            for($j = 1; $j <= mt_rand(2, 5); $j++) {
+                $image = new Image();
+
+                $image->setUrl($faker->imageUrl())
+                      ->setCaption($faker->sentence())
+                      ->setAd($ad);
+
+                $manager->persist($image);
+            }
             
             $manager->persist($ad);
         }
